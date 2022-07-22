e6fc714a05bd2c06eaa1b539c7ac437c77799ec4
김주형 <wnguddl96@knou.ac.kr>
2022-07-22 19:44:52 +0900

    Add files via upload

--- oopAndPatterns/chapter01/Application.java
+++ oopAndPatterns/chapter01/Application.java
@@ -9,6 +9,7 @@ public class Application implements OnClickListner{
     private Button button1 = new Button("button1");
     private Button button2 = new Button("button2");
 
+
     private ScreenUI currentScreen = null;
 
     private String currentMenu = null;
@@ -20,35 +21,59 @@ public class Application implements OnClickListner{
         button2.setOnClickListener(this);
     }
 
-    public void clicked(Component eventSource){
-        if(eventSource.getId().equals("menu1")){
-            changeUIToMenu1();
-        }else if (sourceId.equals("button1")){
-            if(currentScreen == null){
-                return;
-                currentScreen.handleButton1Click();
+    private OnClickListener menuListener = new OnClickListener(){
+        public void clicked(Component eventSource){
+            String sourceId = eventSource.getName();
+            if(eventSource.getName().equals("menu1")){
+                currentScreen = new Menu1ScreenUI();
+            }else if (sourceId.equals("button1")){
+                currentScreen = new Menu2ScreenUI();
             }
+            currentScreen.show();
         }
-        /**
-         else if (eventSource.getId().equals("menu2")){
-         changeUIToMenu2();
-         }else if (eventSource.getId().equals("button1")){
-         if(currentMenu == null)
-         return;
-         if(currentMenu.equals("menu1"))
-         processButton1WhenMenu1();
-         else if (currentMenu.equals("menu2"))
-         processButton1WhenMenu2();
-         }else if (eventSource.getId().equals("button2")){
-         if(currentMenu == null)
-         return;
-         if(currentMenu.equals("menu1"))
-         processButton2WhenMenu1();
-         else if (currentMenu.equals("menu2"))
-         processButton2WhenMenu2();
-         */
+    };
 
-    }
+    private OnClickListener buttonListener = new OnClickListener(){
+        public void clicked(Component eventSource){
+            if(currentScreen == null)
+                return;
+            String sourceId = eventSource.getName();
+        }
+    };
+
+    /**
+     *
+     *public void clicked(Component eventSource){
+     *
+     *         if(eventSource.getName().equals("menu1")){
+     *             changeUIToMenu1();
+     *         }else if (sourceId.equals("button1")){
+     *             if(currentScreen == null){
+     *                 return;
+     *                 currentScreen.handleButton1Click();
+     *             }
+     *         }
+     *
+     *          else if (eventSource.getId().equals("menu2")){
+     *          changeUIToMenu2();
+     *          }else if (eventSource.getId().equals("button1")){
+     *          if(currentMenu == null)
+     *          return;
+     *          if(currentMenu.equals("menu1"))
+     *          processButton1WhenMenu1();
+     *          else if (currentMenu.equals("menu2"))
+     *          processButton1WhenMenu2();
+     *          }else if (eventSource.getId().equals("button2")){
+     *          if(currentMenu == null)
+     *          return;
+     *          if(currentMenu.equals("menu1"))
+     *          processButton2WhenMenu1();
+     *          else if (currentMenu.equals("menu2"))
+     *          processButton2WhenMenu2();
+     *     }
+     */
+
+
 
     public void changeUIToMenu1(){
         currentMenu = "menu1";
